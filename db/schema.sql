1374c4c0fb0590268e9eea8f967e233644b040fe
mackgabe <mackenzieabe@gmail.com>
2022-07-25 10:34:43 -0400

    added employee seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,24 @@
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,  
+    name VARCHAR(30) NOT NULL
 ); 
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
+    department_id INTEGER ,
+    FOREIGN KEY (department_id) REFERENCES department(id)
     salary DECIMAL (10, 2)
-    department_id INTEGER 
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER NOT NULL 
-)
+    title VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    salary DECIMAL (10, 2),
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,29 @@
 INSERT INTO department
 (department_name)
 VALUES
-('Engineering', 'Finance', 'Legal', 'Sales');
+('Engineering', 'Finance', 'Legal', 'Sales'); 
+
+INSERT INTO roles
+(title, department_id, salary)
+VALUES
+('Sales Lead', 'Sales', '100000'),
+('Salesperson', 'Sales', '8000'),
+('Lead Engineer', 'Engineering', '150000'),
+('Software Engineer', 'Engineering', '120000'),
+('Account Manager', 'Finance', '160000'),
+('Accountant', 'Finance', '125000'),
+('Legal Team Lead', 'Legal', '250000'),
+('Lawyer', 'Legal', '190000'); 
+
+INSERT INTO employee
+(first_name, last_name, title, role_id, salary, manager_id)
+VALUES
+('John', 'Doe', 'Sales Lead', '4', '100000', 'null'),
+('Mike', 'Chan', 'Salesperson', '4', '80000', '1'),
+('Ashley', 'Rodriguez', 'Lead Engineer', '1', '150000','null'),
+('Kevin', 'Tupik', 'Software Engineer', '1', '120000', '3'),
+('Kunal', 'Singh', 'Account Manager','2', '160000', 'null'),
+('Malia', 'Brown', 'Accountant', '2', '125000', '5'),
+('Sarah', 'Lourd', 'Legal Team Lead', '3', '250000', 'null'),
+('Tom', 'Allen', 'Lawyer', '3', '190000', '7');
+
